7053749018a3f948a738363e194780b8beb90918
Rafa Santos <rafael.santos.bra@gmail.com>
2022-07-04 22:52:58 -0400

    Skip google tag manager if no id

--- src/google-tag-manager/google-tag-manager.js
+++ src/google-tag-manager/google-tag-manager.js
@@ -1,14 +1,16 @@
 import TagManager from 'react-gtm-module'
 
 const initializeGoogleTagManager = () => {
-    const googleTagManagerId = process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID
- 
-    const tagManagerArgs = {
-        gtmId: googleTagManagerId
-    }
-    
-    console.log('Initializing Google Tag Manager:', tagManagerArgs)
-    TagManager.initialize(tagManagerArgs)    
+  const googleTagManagerId = process.env.REACT_APP_GOOGLE_TAG_MANAGER_ID
+  
+  if (googleTagManagerId === undefined || googleTagManagerId === null || googleTagManagerId === '') return
+
+  const tagManagerArgs = {
+    gtmId: googleTagManagerId
+  }
+
+  console.log('Initializing Google Tag Manager:', tagManagerArgs)
+  TagManager.initialize(tagManagerArgs)
 }
 
 export default initializeGoogleTagManager
