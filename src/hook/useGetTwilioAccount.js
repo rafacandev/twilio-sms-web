97915ac110ddf94a618db64d7ddb057fac728fdc
Rafael Santos <rafa.can.dev@gmail.com>
2024-03-23 22:14:07 -0400

    Removed dead code

--- src/hook/useGetTwilioAccount.js
+++ src/hook/useGetTwilioAccount.js
@@ -1,17 +1,4 @@
-import axios from "axios";
-import {toCredentials} from "../context/AuthenticationProvider";
-
-/**
- * Account information
- */
 export class AccountInfo {
-  /**
-   * @param {string} name
-   * @param {string} type
-   * @param {string} status
-   * @param {Date} dateCreated
-   * @param {Date} dateUpdated
-   */
   constructor(name='', type='', status='', dateCreated=new Date(), dateUpdated=new Date()) {
     this.name = name
     this.type = type
@@ -20,24 +7,3 @@ export class AccountInfo {
     this.dateUpdated = dateUpdated
   }
 }
-
-const useGetTwilioAccount = ({onSuccess=()=>{}, onError=()=>{}, onComplete=()=>{}}) => {
-
-  /**
-   * @param {Authentication} authentication
-   */
-  const getAccount = (authentication) => {
-    const url = `https://api.twilio.com/2010-04-01/Accounts/${authentication.accountSid}.json`
-    axios.get(url,
-      {
-        auth: toCredentials(authentication)
-      })
-      .then(response => onSuccess(response))
-      .catch(error => onError(error))
-      .then(() => onComplete())
-  }
-
-  return getAccount
-}
-
-export default useGetTwilioAccount
