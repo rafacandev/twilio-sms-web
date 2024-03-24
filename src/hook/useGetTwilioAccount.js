228eb4c27074c7f4ed6c55153b4039194f0166ce
Rafael Santos <rafa.can.dev@gmail.com>
2024-03-23 22:24:23 -0400

    Removed dead code

--- src/hook/useGetTwilioAccount.js
+++
@@ -1,9 +0,0 @@
-export class AccountInfo {
-  constructor(name='', type='', status='', dateCreated=new Date(), dateUpdated=new Date()) {
-    this.name = name
-    this.type = type
-    this.status = status
-    this.dateCreated = dateCreated
-    this.dateUpdated = dateUpdated
-  }
-}
