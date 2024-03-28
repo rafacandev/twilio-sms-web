065d0e5103334b43f8d0af6fab765239a8e0bdef
Rafael Santos <rafa.can.dev@gmail.com>
2024-03-28 17:21:17 -0400

    Create sync service when does not exist

--- src/component/NotificationsPage/NotificationsPage.jsx
+++ src/component/NotificationsPage/NotificationsPage.jsx
@@ -2,7 +2,7 @@ import DefaultLayout from '../DefaultLayout/DefaultLayout'
 import { useAuthentication } from "../../context/AuthenticationProvider"
 import { useHistory } from "react-router-dom"
 import axios from "axios";
-import { Authentication, toCredentials } from "../../context/AuthenticationProvider";
+import { Authentication, toCredentials } from "../../context/AuthenticationProvider"
 
 export const NotificationsPage = () => {
   const [authentication] = useAuthentication()
@@ -16,7 +16,15 @@ export const NotificationsPage = () => {
 
   const handleRun = async () => {
     const resp = await axios.get('https://sync.twilio.com/v1/Services?PageSize=20', { auth: toCredentials(authentication) })
-    console.log('Services', resp)
+    const twilioSmsWebService = resp.data.services.filter(s => s.friendly_name === 'twilio_sms_web')[0]
+    console.log(`Searched twilio_sms_web service: ${twilioSmsWebService}`)
+    if (!twilioSmsWebService) {
+      console.log('Did not find twilio_sms_web service, creating one...')
+      const data = new URLSearchParams()
+      data.append('FriendlyName', 'twilio_sms_web')
+      await axios.post('https://sync.twilio.com/v1/Services', data, { auth: toCredentials(authentication) })  
+    }
+    console.log('Services', twilioSmsWebService, resp.data.services)
   }
 
   return <DefaultLayout>
