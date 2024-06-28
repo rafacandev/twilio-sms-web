4b6c0b6a0e91f43b9c6a21f15f2d29b46d70591c
Rafael Santos <rafa.can.dev@gmail.com>
2024-06-27 22:37:49 -0400

    Add getTwilioMessages

--- src/component/NotificationsPage/NotificationsPage.jsx
+++ src/component/NotificationsPage/NotificationsPage.jsx
@@ -1,28 +1,50 @@
 import { DefaultLayout } from "../DefaultLayout/DefaultLayout"
 import { useAuthentication } from "../../context/AuthenticationProvider"
-import { useHistory } from "react-router-dom"
 import { useGetOrCreateTwilioDocument } from "../../hook/useCreateTwilioDocument"
+import { useEffect, useState } from "react"
+import { getTwilioMessages } from "../../core/getTwilioMessages"
+import { MessageCard } from "../MessageCard/MessageCard"
+
+/**
+ * @typedef {import ("../../core/getTwilioMessages").Message} Message
+ */
+
+/**
+ * @param {{messages: Array<Message>}} messages
+ */
+const MessageList = ({ messages = [] }) =>
+  messages.map(m => (
+    <MessageCard
+      body={m.body}
+      date={m.date}
+      direction={m.direction}
+      from={m.from}
+      key={m.messageSid}
+      messageSid={m.messageSid}
+      status={m.status}
+      to={m.to}
+    />
+  ))
 
 export const NotificationsPage = () => {
   const [authentication] = useAuthentication()
-  const history = useHistory()
   const getOrCreateDoc = useGetOrCreateTwilioDocument()
-
-  // TODO: Move this to a router guard
-  if (!authentication?.accountSid) {
-    history.push("/authentication")
-    return null
-  }
+  const [messages, setMessages] = useState([])
 
   const handleRun = async () => {
     const d = await getOrCreateDoc()
     console.log("Twilio Document", d)
   }
 
+  useEffect(() => {
+    getTwilioMessages(authentication).then(setMessages)
+  }, [authentication])
+
   return (
     <DefaultLayout>
       <h4>Notifications</h4>
       <button onClick={handleRun}>Sync Notifications</button>
+      <MessageList messages={messages} />
     </DefaultLayout>
   )
 }
---
+++ src/core/getTwilioMessages.js
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { toCredentials } from "../context/AuthenticationProvider"
+
+const sortByDate = (a, b) => (Date.parse(a.date) > Date.parse(b.date) ? -1 : 1)
+
+/**
+ * @typedef {Object} Message
+ * @prop {string} messageSid
+ * @prop {string} direction
+ * @prop {string} from
+ * @prop {string} to
+ * @prop {string} status
+ * @prop {string} body
+ */
+
+/**
+ *
+ * @param {import { authentication } from "../context/AuthenticationProvider";} authentication
+ * @returns {Message}
+ */
+export const getTwilioMessages = async authentication => {
+  const credentials = toCredentials(authentication)
+  const url = `https://api.twilio.com/2010-04-01/Accounts/${authentication.accountSid}/Messages.json`
+  const response = await axios.get(url, {
+    auth: credentials,
+  })
+
+  return response.data.messages
+    .map(v => ({
+      messageSid: v.sid,
+      direction: v.direction,
+      from: v.from,
+      to: v.to,
+      date: v.date_created,
+      status: v.status,
+      body: v.body,
+    }))
+    .sort(sortByDate)
+}
