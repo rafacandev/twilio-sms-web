6f6ba2463520330a66bd96f75b9c077586bc2100
Rafael Santos <rafa.can.dev@gmail.com>
2024-07-07 20:02:10 -0400

    Moved google-tag-manager to js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import { App } from "./App"
 import { reportWebVitals } from "./reportWebVitals"
-import { initializeGoogleTagManager } from "./google-tag-manager/google-tag-manager"
+import { initializeGoogleTagManager } from "./js/google-tag-manager"
 
 initializeGoogleTagManager()
 
