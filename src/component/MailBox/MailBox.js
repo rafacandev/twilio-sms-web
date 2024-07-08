a49fb0ca33c3733c8e63fb91c0160d2b9303e4dc
Rafael Santos <rafa.can.dev@gmail.com>
2024-07-07 23:06:47 -0400

    Basic DockedLayout

---
+++ src/component/MailBox/MailBox.js
@@ -0,0 +1,9 @@
+import { DockedLayout } from "../DockedLayout/DockedLayout"
+
+export const MailBox = () => {
+  return (
+    <DockedLayout>
+      <div>Your mail box is empty!</div>
+    </DockedLayout>
+  )
+}
