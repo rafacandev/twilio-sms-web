057b981460521732803a631171da075aff9bb14e
Rafael Santos <rafa.can.dev@gmail.com>
2024-07-09 16:55:37 -0400

    DockedLayout navbar icons

--- src/component/DockedLayout/DockedLayout.js
+++ src/component/DockedLayout/DockedLayout.js
@@ -3,26 +3,48 @@ import { GithubOutlined, InboxOutlined, SendOutlined, PhoneOutlined } from "@ant
 const iconStyle = { fontSize: ".9rem", lineHeight: "1.5rem" }
 
 const NavItem = ({ children }) => (
-  <div
-    className="flex flex-col items-center justify-center h-12 px-3 mx-2
-  hover:bg-violet-700 border-2 border-violet-900 hover:border-violet-600 hover:cursor-pointer"
-  >
-    {children}
-  </div>
+  <div className="h-12 flex flex-col px-4 mx-2 hover:bg-violet-700 hover:cursor-pointer">{children}</div>
+)
+
+const NavBar = () => (
+  <nav className="text-white px-5 flex">
+    <NavItem>
+      <InboxOutlined style={iconStyle} />
+      Inbox
+    </NavItem>
+    <NavItem>
+      <SendOutlined style={iconStyle} />
+      Sent
+    </NavItem>
+
+    <NavItem>
+      <PhoneOutlined style={iconStyle} />
+      Phones
+    </NavItem>
+
+    <div className="grow flex justify-center items-center text-lg">Twilio SMS Web</div>
+
+    <NavItem>
+      <GithubOutlined style={iconStyle} />
+      GitHub
+    </NavItem>
+  </nav>
 )
 
 export const DockedLayout = ({ children }) => {
   return (
     <div className="flex flex-col h-full">
       <div className="flex h-12">
-        <div className="bg-blue-100 grow">a</div>
-        <div className="bg-blue-200 w-full max-w-xs">b</div>
-        <div className="bg-blue-300 grow">c</div>
+        <div className="bg-violet-900 grow"></div>
+        <div className="bg-violet-900 w-full max-w-4xl">
+          <NavBar />
+        </div>
+        <div className="bg-violet-900 grow"></div>
       </div>
       <div className="flex grow">
-        <div className="bg-blue-100 grow">a</div>
-        <div className="bg-blue-200 w-full max-w-xs">b</div>
-        <div className="bg-blue-300 grow">c</div>
+        <div className="bg-gray-200 grow"></div>
+        <div className="bg-gray-100 w-full max-w-4xl p-2 pb-4">{children}</div>
+        <div className="bg-gray-200 grow"></div>
       </div>
     </div>
   )
