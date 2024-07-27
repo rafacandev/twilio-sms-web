413d2b48064e47103fd3f7522168eb26ebeb3ab3
Rafael Santos <rafa.can.dev@gmail.com>
2024-07-26 21:47:13 -0400

    DockedLayout navigate to mailbox

--- src/component/DockedLayout/DockedLayout.js
+++ src/component/DockedLayout/DockedLayout.js
@@ -1,35 +1,39 @@
-import { GithubOutlined, InboxOutlined, SendOutlined, PhoneOutlined } from "@ant-design/icons"
+import { GithubOutlined, InboxOutlined, SendOutlined } from "@ant-design/icons"
+import { useHistory } from "react-router-dom"
 
-const iconStyle = { fontSize: ".9rem", lineHeight: "1.5rem" }
-
-const NavItem = ({ children }) => (
-  <div className="h-12 flex flex-col px-3 mx-2 hover:bg-violet-700 hover:cursor-pointer">{children}</div>
+const NavItem = ({ children, onClick = () => {} }) => (
+  <div onClick={onClick} className="h-12 flex flex-col px-3 mx-2 hover:bg-violet-700 hover:cursor-pointer">
+    {children}
+  </div>
 )
 
-const NavBar = () => (
-  <nav className="text-white px-2 flex">
-    <NavItem>
-      <InboxOutlined style={iconStyle} />
-      Inbox
-    </NavItem>
-    <NavItem>
-      <SendOutlined style={iconStyle} />
-      Sent
-    </NavItem>
+const NavBar = () => {
+  const history = useHistory()
 
-    <NavItem>
-      <PhoneOutlined style={iconStyle} />
-      Phones
-    </NavItem>
+  const navigateToInbox = () => {
+    history.push("/mailbox")
+  }
 
-    <div className="grow flex justify-center items-center text-lg">Twilio SMS Web</div>
-
-    <NavItem>
-      <GithubOutlined style={iconStyle} />
-      GitHub
-    </NavItem>
-  </nav>
-)
+  return (
+    <>
+      <nav className="text-white text-[.75rem] px-2 flex">
+        <NavItem onClick={navigateToInbox}>
+          <InboxOutlined className="text-lg" />
+          Mailbox
+        </NavItem>
+        <NavItem>
+          <SendOutlined className="text-lg" />
+          Send
+        </NavItem>
+        <div className="grow flex justify-center items-center text-lg">Twilio SMS Web</div>
+        <NavItem>
+          <GithubOutlined className="text-lg" />
+          GitHub
+        </NavItem>
+      </nav>
+    </>
+  )
+}
 
 export const DockedLayout = ({ children }) => {
   return (
