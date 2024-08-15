0979cd44559c9b457fe6f9f1bd96589503531086
Rafael Santos <rafa.can.dev@gmail.com>
2024-08-14 22:16:15 -0400

    Added state for message filter

--- src/component/Inbox/Inbox.jsx
+++ src/component/Inbox/Inbox.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react"
 import { DockedLayout } from "../DockedLayout/DockedLayout"
 import { getTwilioMessages } from "../../js/getTwilioMessages"
 import { MessageRows } from "../MessageRows/MessageRows"
-import { Selector } from "./Selector"
+import { MessageFilterOption, Selector } from "./Selector"
 import { getTwilioPhoneNumbers } from "../../js/getTwilioPhoneNumbers"
 
 export const Inbox = () => {
   const [messages, setMessages] = useState([])
   const [phoneNumbers, setPhoneNumbers] = useState([])
   const [loading, setLoading] = useState(true)
+  const [messageFilter, setMessageFilter] = useState(MessageFilterOption.all)
 
   useEffect(() => {
     getTwilioMessages().then(setMessages)
@@ -17,11 +18,13 @@ export const Inbox = () => {
       .then(() => setLoading(false))
   }, [])
 
+  console.log('messageFilter', messageFilter)
+
   return (
     <DockedLayout>
       <h3>Inbox</h3>
       <p className="text-xs italic">Your most recent incoming messages</p>
-      <Selector className="mb-1" phoneNumbers={phoneNumbers} loading={loading} />
+      <Selector className="mb-1" phoneNumbers={phoneNumbers} loading={loading} onMessageFilterChange={setMessageFilter} />
       <MessageRows messages={messages} />
     </DockedLayout>
   )
--- src/component/Inbox/Selector.jsx
+++ src/component/Inbox/Selector.jsx
@@ -1,27 +1,35 @@
 import { SelectAutoComplete } from "../SelectAutoComplete/SelectAutoComplete"
 import { maskPhoneNumber } from "../PhoneNumberSelector/PhoneNumberSelector"
 import { select, selectOptions } from "../../ui/classes"
+import { emptyFn } from "../../js/types"
 
 const toOptions = (phoneNumbers = []) => {
   const options = phoneNumbers.map(p => ({ val: p, text: maskPhoneNumber(p) }))
   return [{ val: "all", text: "All phone numbers" }, ...options]
 }
 
-export const Selector = ({ className = "", phoneNumbers = [], loading = true }) => {
+export const MessageFilterOption = {
+  all: 'all',
+  recent: 'recent',
+  received: 'received',
+  sent:'sent'
+}
+
+export const Selector = ({ className = "", phoneNumbers = [], loading = true, onMessageFilterChange = emptyFn }) => {
   return (
     <div className={className}>
       <div className="flex gap-1">
-        <select defaultValue="recent" className={`${select}`}>
-          <option className={selectOptions} value="recent">
+        <select defaultValue="recent" className={`${select}`} onChange={e => onMessageFilterChange(e.target.value)}>
+          <option className={selectOptions} value={MessageFilterOption.recent}>
             Recent messages
           </option>
-          <option className={selectOptions} value="received">
+          <option className={selectOptions} value={MessageFilterOption.received}>
             Received messages
           </option>
-          <option className={selectOptions} value="sent">
+          <option className={selectOptions} value={MessageFilterOption.sent}>
             Sent messages
           </option>
-          <option className={selectOptions} value="all">
+          <option className={selectOptions} value={MessageFilterOption.all}>
             All messages
           </option>
         </select>
