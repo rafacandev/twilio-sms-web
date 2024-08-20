4cff4816fb167d836fcfe6bbd09d93bfc1e44021
Rafael Santos <rafa.can.dev@gmail.com>
2024-08-19 23:07:04 -0400

    Add from and to params to getTwilioPhoneNumber

--- src/js/getTwilioMessages.js
+++ src/js/getTwilioMessages.js
@@ -5,6 +5,16 @@ import { getAuthentication, toCredentials } from "../context/AuthenticationProvi
  * @typedef {import('./types').Message} Message
  */
 
+const toMessage = (v={}) => ({
+  messageSid: v.sid,
+  direction: v.direction,
+  from: v.from,
+  to: v.to,
+  date: v.date_created,
+  status: v.status,
+  body: v.body,
+})
+
 const sortByDate = (a, b) => (Date.parse(a.date) > Date.parse(b.date) ? -1 : 1)
 
 /**
@@ -26,38 +36,33 @@ export const getTwilioMessagesByPhoneNumber = async phoneNumber => {
   return []
     .concat(fromResult.data.messages)
     .concat(toResult.data.messages)
-    .map(v => ({
-      messageSid: v.sid,
-      direction: v.direction,
-      from: v.from,
-      to: v.to,
-      date: v.date_created,
-      status: v.status,
-      body: v.body,
-    }))
+    .map(toMessage)
     .sort(sortByDate)
 }
 
 /**
  * @returns {Promise<Array<Message>>}
  */
-export const getTwilioMessages = async () => {
+export const getTwilioMessages = async (from = '', to = '') => {
   const authentication = getAuthentication()
   const credentials = toCredentials(authentication)
   const url = `https://api.twilio.com/2010-04-01/Accounts/${authentication.accountSid}/Messages.json`
+
+  const params = {}
+  if (to.length > 0) {
+    params.To = to
+  }
+  
+  if (from.length > 0) {
+    params.From = from
+  }
+
   const response = await axios.get(url, {
     auth: credentials,
+    params
   })
 
   return response.data.messages
-    .map(v => ({
-      messageSid: v.sid,
-      direction: v.direction,
-      from: v.from,
-      to: v.to,
-      date: v.date_created,
-      status: v.status,
-      body: v.body,
-    }))
+    .map(toMessage)
     .sort(sortByDate)
 }
