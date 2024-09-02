22ef1df1b431dbbb4849d82b8abb49280135e59d
Rafael Santos <rafa.can.dev@gmail.com>
2024-09-02 19:41:53 -0400

    feat: fetch messages on conversation page

--- src/component/ConversationPage/ConversationPage.jsx
+++ src/component/ConversationPage/ConversationPage.jsx
@@ -1,8 +1,27 @@
-import { useLocation, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
+import { DockedLayout } from "../DockedLayout/DockedLayout"
+import { useEffect, useState } from "react"
+import { getTwilioMessages, sortByDate } from "../../js/getTwilioMessages"
+import { MessageRows } from "../MessageRows/MessageRows"
 
-export const ConversationPage = props => {
-  const par = useParams()
-  const loc = useLocation()
-  console.log("conv", par, loc, props)
-  return <>Conversation Page </>
+export const ConversationPage = () => {
+  const { from, to } = useParams()
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    const ft = getTwilioMessages(from, to)
+    const tf = getTwilioMessages(to, from)
+    Promise.all([ft, tf]).then(msg => setMessages(msg.flat().sort(sortByDate)))
+  }, [from, to])
+
+  return (
+    <DockedLayout>
+      <h3>Conversation</h3>
+      <p className="text-xs">
+        Messages exchanged between <span className="font-semibold">{from}</span> and{" "}
+        <span className="font-semibold">{to}</span>
+      </p>
+      <MessageRows messages={messages} />
+    </DockedLayout>
+  )
 }
