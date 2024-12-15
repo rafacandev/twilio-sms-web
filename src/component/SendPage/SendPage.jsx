6c1d27b1988c4d3e261bc276727740515ed3d602
Rafael Santos <rafacandev@gmail.com>
2024-12-15 02:24:47 +0000

    feat(SendPage): load phone numbers

--- src/component/SendPage/SendPage.jsx
+++ src/component/SendPage/SendPage.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react"
 import { DockedLayout } from "../DockedLayout/DockedLayout"
 import { PhoneSelector } from "../PhoneSelector/PhoneSelector"
+import { getTwilioPhoneNumbers } from "../../js/getTwilioPhoneNumbers"
 
 export const SendPage = () => {
+  const [phoneNumbers, setPhoneNumbers] = useState([])
+  const [phoneNumber, setPhoneNumber] = useState("default")
+  const [isPhoneNumberLoading, setPhoneNumbersLoading] = useState(true)
+
+  useEffect(() => {
+    getTwilioPhoneNumbers()
+      .then(setPhoneNumbers)
+      .then(() => setPhoneNumbersLoading(false))
+  }, [])
+
   return (
     <DockedLayout>
       <h3>Send</h3>
       <p className="text-xs italic">Send a message to a phone number.</p>
-      <PhoneSelector phoneNumbers={[]} />
+      <PhoneSelector phoneNumbers={phoneNumbers} phoneNumber={phoneNumber} onPhoneNumberChange={setPhoneNumber} loading={isPhoneNumberLoading} />
     </DockedLayout>
   )
 }
