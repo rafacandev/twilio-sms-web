a8d0572031ba6fefe3057af43c4a9338059e0b6e
Rafael Santos <rafacandev@gmail.com>
2024-12-16 01:51:23 +0000

    feat(SendPage): add send button

--- src/component/SendPage/SendPage.jsx
+++ src/component/SendPage/SendPage.jsx
@@ -3,11 +3,10 @@ import { DockedLayout } from "../DockedLayout/DockedLayout"
 import { PhoneSelector } from "../PhoneSelector/PhoneSelector"
 import { getTwilioPhoneNumbers } from "../../js/getTwilioPhoneNumbers"
 
-const messageValidationPattern = "[\\w\\d]{3,}"
-
 export const SendPage = () => {
   const [phoneNumbers, setPhoneNumbers] = useState([])
-  const [phoneNumber, setPhoneNumber] = useState("default")
+  const [from, setFrom] = useState("default")
+  const [to, setTo] = useState("")
   const [isPhoneNumberLoading, setPhoneNumbersLoading] = useState(true)
 
   useEffect(() => {
@@ -20,24 +19,40 @@ export const SendPage = () => {
     <DockedLayout>
       <h3>Send</h3>
       <p className="text-xs italic">Send a message to a phone number.</p>
-      <PhoneSelector
-        phoneNumbers={phoneNumbers}
-        phoneNumber={phoneNumber}
-        onPhoneNumberChange={setPhoneNumber}
-        loading={isPhoneNumberLoading}
-      />
+      <div className="flex items-center">
+        <label className="w-10">From:</label>{" "}
+        <PhoneSelector
+          className="inline w-36"
+          phoneNumbers={phoneNumbers}
+          phoneNumber={from}
+          onPhoneNumberChange={setFrom}
+          loading={isPhoneNumberLoading}
+        />
+      </div>
+      <div className="flex items-center mt-2">
+        <label className="w-10">To:</label>
+        <input
+          className="w-36 border-2 rounded p-1 border-violet-200 invalid:border-red-500 invalid:border-red"
+          type="tel"
+          pattern="[\+]\d{10}"
+          onChange={i => setTo(i.target.value)}
+        />
+      </div>
+
       <p className="mb-1 mt-4">
-        Sending a message to <b>{phoneNumber}</b>
+        Sending a message from <b className="mx-1">{from}</b> to <b className="mx-1">{to}</b>
       </p>
       <textarea
-        className="w-full bg-white border-2 rounded p-2 border-violet-200 invalid:border-red-500"
-        placeholder={`Send a message to ${phoneNumber}`}
-        required
+        className="w-full border-2 rounded p-2 border-violet-200 invalid:border-red-500"
+        placeholder={`Send a message from  ${from}  to  ${to}`}
         minLength="3"
         maxLength="500"
         rows="5"
       ></textarea>
-      <p className="text-xs text-right font-thin">Messages must be between 3 and 500 characters.</p>
+      <p className="text-xs font-thin m-0">Messages must be between 3 and 500 characters.</p>
+      <button className="border-2 rounded py-2 px-4 border-white invalid:border-red-500 bg-purple-900 text-white hover:bg-purple-700 active:bg-purple-950 float-right">
+        Send
+      </button>
     </DockedLayout>
   )
 }
