0aa2be4fe75ca1217d11d804318f9fab2b0d7978
Rafael Santos <rafacandev@gmail.com>
2025-01-12 18:00:12 -0500

    style: fixes #15 mobile responsiveness

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,10 +1,10 @@
 import { GithubOutlined, InboxOutlined, SendOutlined } from "@ant-design/icons"
 import { useNavigate } from "react-router-dom"
 
-const NavItem = ({ children, onClick = () => {} }) => (
+const NavItem = ({ className, children, onClick = () => {} }) => (
   <div
     onClick={onClick}
-    className="h-14 flex flex-col justify-center items-center px-3 mx-2 hover:bg-violet-700 hover:cursor-pointer"
+    className={`h-14 flex flex-col justify-center items-center px-4 hover:bg-violet-700 hover:cursor-pointer ${className}`}
   >
     {children}
   </div>
@@ -23,18 +23,18 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="text-white px-2 flex">
+      <nav className="flex text-white px-1 sm:px-4">
         <NavItem onClick={navigateToInbox}>
-          <InboxOutlined className="text-lg mt-1" />
+          <InboxOutlined className="text-lg" />
           <span className="mt-1">Inbox</span>
         </NavItem>
         <NavItem onClick={navigateToSend}>
-          <SendOutlined className="text-lg mt-1" />
+          <SendOutlined className="text-lg" />
           <span className="mt-1">Send</span>
         </NavItem>
-        <div className="grow flex justify-center items-center text-lg mt-1">Twilio SMS Web</div>
-        <NavItem>
-          <GithubOutlined className="text-lg mt-1" />
+        <div className="grow flex justify-center items-center text-lg">Twilio SMS Web</div>
+        <NavItem className="hidden sm:flex">
+          <GithubOutlined className="text-lg" />
           <span className="mt-1">GitHub</span>
         </NavItem>
       </nav>
@@ -42,36 +42,31 @@ const NavBar = () => {
   )
 }
 
-export const Layout = ({ children }) => {
-  return (
-    <div className="flex flex-col h-full">
-      <div className="flex h-14">
-        <div className="bg-violet-900 grow"></div>
-        <div className="bg-violet-900 w-full max-w-4xl">
-          <NavBar />
-        </div>
-        <div className="bg-violet-900 grow"></div>
-      </div>
-      <div className="flex grow">
-        <div className="bg-gray-200 grow"></div>
-        <div className="bg-gray-100 w-full max-w-4xl pt-2 pb-4 md:px-2 lg:px-4">{children}</div>
-        <div className="bg-gray-200 grow"></div>
-      </div>
+export const Layout = ({ children }) => (
+  <div className="flex flex-col min-h-full bg-gray-200">
+    <div className="bg-violet-900 flex justify-center">
+      <span className="block h-14 w-full max-w-screen-lg">
+        <NavBar />
+      </span>
     </div>
-  )
-}
+    <div className="grow flex justify-center">
+      <span className="block bg-gray-50 w-full max-w-screen-lg p-1 sm:p-4">{children}</span>
+    </div>
+    <div className="bg-violet-900 flex justify-center">
+      <span className="block w-full max-w-screen-lg h-14">Footer</span>
+    </div>
+  </div>
+)
 
-export const LayoutWithoutNavBar = ({ children }) => {
-  return (
-    <div className="flex flex-col h-full">
-      <div className="flex h-14">
-        <div className="bg-violet-900 grow flex justify-center items-center text-lg text-white">Twilio SMS Web</div>
-      </div>
-      <div className="flex grow">
-        <div className="bg-gray-200 grow"></div>
-        <div className="bg-gray-100 w-full max-w-4xl pt-2 pb-4 md:px-2 lg:px-4">{children}</div>
-        <div className="bg-gray-200 grow"></div>
-      </div>
+export const LayoutWithoutNavBar = ({ children }) => (
+  <div className="flex flex-col h-full">
+    <div className="flex h-14">
+      <div className="bg-violet-900 grow flex justify-center items-center text-lg text-white">Twilio SMS Web</div>
     </div>
-  )
-}
+    <div className="flex grow">
+      <div className="bg-gray-200 grow"></div>
+      <div className="bg-gray-100 w-full max-w-screen-md pt-2 pb-4 px-4">{children}</div>
+      <div className="bg-gray-200 grow"></div>
+    </div>
+  </div>
+)
